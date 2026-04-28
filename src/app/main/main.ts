f69dc7a907b16d88525650407703ab4e6b0d1e97
nabil kadjouh <nabil.kadjouh@gmail.com>
2026-04-28 11:33:37 +0200

    barre de recherche formation added

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 // import { Formation } from '../model/user.model';
 import { FormationService } from '../services/formation';
@@ -14,17 +15,31 @@ interface Formation {
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './main.html',
   styleUrls: ['./main.css']
 })
 export class MainComponent implements OnInit {
   formations: Formation[] = [];
+  filteredFormations: Formation[] = [];
+  searchTerm: string = '';
 
   constructor(private formationService: FormationService, private router: Router) {}
 
   ngOnInit() {
     this.formations = this.formationService.getFormations();
+    this.filteredFormations = this.formations;
+  }
+
+  onSearch() {
+    if (this.searchTerm.trim() === '') {
+      this.filteredFormations = this.formations;
+    } else {
+      this.filteredFormations = this.formations.filter(formation =>
+        formation.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        formation.description.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 
   voirFormation(id: number) {
